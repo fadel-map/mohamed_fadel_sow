19101d5c25824a8ac78f8971db068cf1a1b0ca67
fadel-map <mofadelsow@gmail.com>
2026-02-18 04:07:12 +0100

    Add files via upload

---
+++ fadelcv.py
@@ -0,0 +1,51 @@
+import streamlit as st
+
+
+
+st.title("🔔PROFIL")
+st.subheader(" 🧠 Jeune technicien supérieur en géomatique 🌍 et en économie 🧭.")
+st.write(" 🧠 Passionné par la recherche, l'innovation et le développement technologique 🖥️")
+
+
+
+st.title("🔔EXPERIENCE")
+st.write("🔗2026:Participation a de nombreux projets de collecte de données Arret de bus, Stations, lampadaires")
+st.write("🔗2026:Participation a l'élaboration d'une base de données des Non Fly Zones su Sénégal")
+st.write("🔗2025:Réalisation d'un rapport de recherche sur la gestion financiere des ONG")
+st.write("🔗2024:Participation a l'analyse économique du référentiel SENEGAL HORIZON 2050")
+
+
+
+
+st.title("🔔COMPETENCES")
+st.write("✅Bureautique")
+st.write("✅SIG")
+st.write("✅Developpeur SIG")
+st.write("✅Python(Notebook, JupyterLab)")
+st.write("✅Analyse statistique et gestion des projets")
+st.write("✅Intelligence économique")
+st.write("✅Analyse et évaluation des politiques publiques")
+
+
+
+
+
+st.title("🔔AUTRES")
+st.write("📝Nationalité:Sénégalais")
+st.write("⏳Age:20ans")
+st.write("👩🏻‍❤️‍💋‍👨🏻Situation:Célibataire")
+st.write("✅Langues:Francais__Anglais")
+
+
+
+with st.sidebar:
+    st.title("⭐ MOHAMED FADEL SOW")
+    st.image("fadel.png")
+    st.write("✉️fadelsow581@gmail.com")
+    st.write("📞+221 70 977 24 31")
+    st.write("📞+221 78 437 89 15")
+    st.write("🌐 SINDIA, MBOUR, SENEGAL")
+
+        
+
+
